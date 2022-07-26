3091518576442f7e51baab419d29a52e7a4a651e
BEAR小熊帽 <1922343892@qq.com>
2022-07-26 15:45:05 +0800

    init

---
+++ src/views/Article/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <comment-out-page>
+      <template #main>
+        <div>
+          <detail-content></detail-content>
+          <el-divider></el-divider>
+          <List></List>
+        </div>
+      </template>
+    </comment-out-page>
+  </div>
+</template>
+
+<script setup>
+import CommentOutPage from "@/components/outPage/CommentOutPage.vue"
+import DetailContent from "@/components/listContent/DetailContent.vue"
+import List from "@/views/Home/component/List.vue"
+</script>
+<style lang="scss" scoped>
+.container {
+  padding: 24px;
+}
+</style>
