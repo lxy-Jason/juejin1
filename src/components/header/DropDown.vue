a54dcca7cd7ed0126516f8013f56937323ce1465
BEAR小熊帽 <1922343892@qq.com>
2022-08-08 16:56:03 +0800

    沸点右侧边页和创作者中心菜单

---
+++ src/components/header/DropDown.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-wrap items-center">
+    <el-dropdown split-button type="primary" @click="handleClick"  trigger="click">
+      创作者中心
+      <template #dropdown>
+      <div>
+        <el-dropdown-menu>
+          <el-dropdown-item><i class="fa fa-pencil" aria-hidden="true"></i> 写文章</el-dropdown-item>
+          <el-dropdown-item><i class="fa fa-gg-circle" aria-hidden="true"></i>发沸点</el-dropdown-item>
+          <el-dropdown-item><i class="fa fa-code" aria-hidden="true"></i>写代码</el-dropdown-item>
+        </el-dropdown-menu>
+        </div>
+      </template>
+    </el-dropdown>
+  </div>
+</template>
+
+<script setup>
+import { ArrowDown } from '@element-plus/icons-vue'
+
+const handleClick = () => {
+  // eslint-disable-next-line no-alert
+  alert('别急，急也没用')
+}
+</script>
+<style scoped>
+.example-showcase .el-dropdown + .el-dropdown {
+  margin-left: 15px;
+}
+.example-showcase .el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+}
+</style>
--- src/views/Boiling/index.vue
+++ src/views/Boiling/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
+  <Header></Header>
     <boiling-out-page>
       <template #sideleft>
+      <MenuSidebar></MenuSidebar>
       </template>
       <template #main>
         <issue></issue>
@@ -14,6 +16,8 @@
 
 <script setup>
 import BoilingOutPage from "@/components/outPage/BothOutPage.vue"
+import MenuSidebar from "@/components/side/MenuSidebar.vue"
 import Issue from "./components/Issue.vue"
+import Header from "@/components/header/Header.vue"
 </script>
 <style lang="scss" scoped></style>
